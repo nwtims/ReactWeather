0a0ea9bf4f1c8b44a533cb9f843fa1677d86536b
Tim Sessions <pdxtim@gmail.com>
2016-06-07 12:18:11 -0700

    Initial commit

---
+++ app/components/Weather.jsx
@@ -0,0 +1,58 @@
+var React = require('react');
+var WeatherMessage = require('WeatherMessage');
+var WeatherForm = require('WeatherForm');
+var openWeatherMap = require('openWeatherMap');
+
+var Weather = React.createClass({
+  // getDefaultProps: function () {
+  //   return {
+  //     location: 'tualatin'
+  //   };
+  // },
+
+  handleSearch: function  (location) {
+    var that = this;
+
+    this.setState({isLoading: true})
+debugger;
+    openWeatherMap.getTemp(location).then(function (temp) {
+      that.setState({
+        location: location,
+        temp: temp,
+        isLoading: false
+      });
+  }, function (errorMessage) {
+      that.setState({isLoading: false});
+     alert(errorMessage);
+  });
+},
+
+  getInitialState: function () {
+    return {
+      isLoading: false
+    }
+  },
+
+  render: function () {
+    // var location = this.state.location;
+    // var temp = this.state.temp;
+    var {isLoading, temp, location} = this.state;
+
+    function renderMessage() {
+      if (isLoading) {
+        return <h3>Fetching weather...</h3>;
+      } else if (temp && location) {
+        return <WeatherMessage location={location} temp={temp}/>;
+      }
+    }
+
+    return (
+      <div>
+        <WeatherForm onSearch={this.handleSearch}/>
+        {renderMessage()}
+      </div>
+    );
+  }
+});
+
+module.exports = Weather;
---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+// var WeatherMessage = React.createClass({
+//   render: function () {
+//     // var location = this.props.location;
+//     // var temp = this.props.temp;
+//     var {temp, location} = this.props;
+//
+//     return(
+//       <div>
+//         <h1>The temperature in {location} is {temp} degrees!</h1>
+//       </div>
+//     )
+//   }
+// });
+
+var WeatherMessage = ({temp, location}) => {
+  // var {temp, location} = props;
+
+  return(
+    <div>
+      <h1>The temperature in {location} is {temp} degrees!</h1>
+    </div>
+  );
+};
+
+module.exports = WeatherMessage;
