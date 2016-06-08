07ebe0bb3ff66c6238df392b22d77cdf05051fd9
Tim Sessions <pdxtim@gmail.com>
2016-06-08 09:06:45 -0700

    added error modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var WeatherMessage = require('WeatherMessage');
 var WeatherForm = require('WeatherForm');
+var ErrorModal = require('ErrorModal');
 var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
@@ -10,10 +11,19 @@ var Weather = React.createClass({
   //   };
   // },
 
-  handleSearch: function  (location) {
+  getInitialState: function () {
+    return {
+      isLoading: false
+    }
+  },
+
+  handleSearch: function (location) {
     var that = this;
 
-    this.setState({isLoading: true})
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
 // debugger;
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
@@ -21,22 +31,19 @@ var Weather = React.createClass({
         temp: temp,
         isLoading: false
       });
-  }, function (errorMessage) {
-      that.setState({isLoading: false});
-     alert(errorMessage);
+  }, function (e) {
+      that.setState({
+        isLoading: false,
+        errorMessage: e.message
+      });
   });
 },
 
-  getInitialState: function () {
-    return {
-      isLoading: false
-    }
-  },
 
   render: function () {
     // var location = this.state.location;
     // var temp = this.state.temp;
-    var {isLoading, temp, location} = this.state;
+    var {isLoading, temp, location, errorMessage} = this.state;
 
     function renderMessage() {
       if (isLoading) {
@@ -46,11 +53,21 @@ var Weather = React.createClass({
       }
     }
 
+    function renderError () {
+      if (typeof errorMessage === 'string') {
+        return (
+          <ErrorModal message={errorMessage}/>
+        )
+      }
+    }
+
+
     return (
       <div>
         <h1 className="text-center">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
+        {renderError()}
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
       WeatherForm: 'app/components/WeatherForm.jsx',
       WeatherMessage: 'app/components/WeatherMessage.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx',
-      About: 'app/components/About.jsx'
+      About: 'app/components/About.jsx',
+      ErrorModal: 'app/components/ErrorModal.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
